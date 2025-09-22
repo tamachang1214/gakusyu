102c80f5b786abca1751051b2bb6ef5160ce74d3
tamachang1214 <tamakasu1214@gmail.com>
2025-09-23 00:06:51 +0900

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+app.use(express.static("public"));
+
+//mysqlの接続情報設定
+const connection = mysql.createConnection({
+  //データベース名、パスワード等を書く
+  host: "localhost",
+  user: "root",
+  password: "test",
+  database: "butaman",
+});
+
+// '/' ホームページを返す
+app.get("/", (req, res) => {
+  res.render("form.ejs");
+  //ここにクエリを書く
+});
+
+//連なるページ作成時には以下のように記載
+app.get("/top", (req, res) => {
+  res.render("top.ejs");
+});
+
+app.listen(3000);
